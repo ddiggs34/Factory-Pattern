5f4141ce1d0721e7138ec4b420d8ed6a1e6d30f8
David Diggs <ddiggs34@gmail.com>
2023-10-16 14:51:27 -0400

    updated

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numWheels;
+
+            Console.WriteLine("How many wheels are on your current vehicle?");
+
+            var userInput = int.TryParse(Console.ReadLine(), out numWheels);
+
+
+            //classname.static method()
+
+            var vehicle = VehicleFactory.GetVehicle(numWheels);
+            vehicle.Drive();
         }
     }
 }
