b0f506e815aebf1a8ce9149e6ca5b593a7c275c8
David Diggs <ddiggs34@gmail.com>
2023-10-16 12:15:49 -0400

    done

---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int numOfWheels)
+        {
+            switch (numOfWheels)
+            {
+
+                case 4:
+                    return new Car(); //will return a type of IVehicle
+                case 2:
+                    return new Motorcycle();
+                default:
+                    return new Car();
+            }
+        }
+    }
+}
